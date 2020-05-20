55b9cecffdcdba7da8f6d19cd12c04cc96ab1285
UpcraftLP <j-schirra@gmx.de>
2020-05-20 17:08:52 +0200

    apply semistandard to clientside scripts

---
+++ src/client/scripts/submit.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const form = document.getElementById('form-submit');
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // const form = document.getElementById('form-submit');
+    const text = document.getElementById('input-text').value;
+    const attachment = document.getElementById('input-attachment').value;
+
+    const xhr = new XMLHttpRequest();
+    const url = `${window.location.protocol}//${window.location.host}/api/v1/post/create`;
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    const data = JSON.stringify({
+      text: text,
+      attachment: attachment
+    });
+    xhr.send(data);
+  });
+}
